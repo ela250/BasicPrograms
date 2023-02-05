3415856d1d6d486e52f9bba8dd78abfb3d6f42bc
elamparethi <elamparethi02a2gmail.com>
2023-02-05 21:48:47 +0530

    Added  PowerOf2 Java Program

---
+++ PowerOf2.java
@@ -0,0 +1,23 @@
+package com.BasicCoreprograms;
+import java.util.Scanner;
+
+public class PowerOf2 {
+    public static void main(String[] args) {
+        int power, sum = 1;
+        Scanner scan = new Scanner(System.in);    // This statement will capture the user input
+        System.out.println("Enter the power");
+        power = scan.nextInt();                   //Captured input would be stored in inputValue
+
+        if (power > 31) {                        // Logic to print the powers of two
+            System.out.println("Value of number should be less then 31");
+            return;
+        } else {
+            System.out.println(sum);
+            for (int i = 1; i <= power; i++) {
+                sum = sum * 2;
+                System.out.println()
+            }
+
+        }
+    }
+}
